799e7dcf45bbb9e0abdfe7ea0b8df3a25bfa6279
Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
2023-05-03 12:34:05 +0000

    Do not let per-db worker go into long wait while parting
    While parting a node from BDR group, per-db worker on the node
    kills/terminates worker processes (WAL senders and apply workers)
    and, where possible, drops replication slots for parted peers.
    Per-db worker sends the worker process SIGTERM to terminate, sets
    per-db worker's latch and continues processing for other worker
    processes. Reason for setting per-db worker's latch after
    terminating a worker processes is to ensure the per-db worker
    will not go into a long wait (180 sec) in bdr_perdb_worker_main()
    and will come very soon to bdr_maintain_db_workers() to drop the
    replication slots of the terminated process. But, the latch that
    was set in bdr_maintain_db_workers() after terminating a worker
    process can get reset by ConditionVariablePrepareToSleep() or
    ConditionVariableSleep() (called via ReplicationSlotDrop() ->
    ReplicationSlotAcquire()). This makes the per-db worker go into
    long wait which is supposed to come very soon to
    bdr_maintain_db_workers() to drop the replication slots. When this
    happens, the parting TAP tests will be occasionally failing with
    [1].
    
    There are 2 ways to fix this issue:
    1. Reduce per-db worker's default wait time from 180 sec to say 10
    sec. While this fix works very well, the per-db worker will be
    doing more spins i.e. more calls to bdr_maintain_db_workers().
    Despite many calls to bdr_maintain_db_workers(), the per-db worker
    ensures it registers only the required apply workers, but it will
    do more work. Incidentally, with this fix, not only the occasional
    replication slot drop failure [1] gets fixed, but also the overall
    BDR tests times have been reduced by more than 50%.
    
    2. Ensure the per-db worker's latch is set at the end of
    bdr_maintain_db_workers() (after ReplicationSlotDrop()) if
    at least one worker processes got terminated. This makes per-db
    worker not go into long wait, but quickly come to
    bdr_maintain_db_workers() again to drop replication slots. Note
    the fact that when a latch is set, WaitLatch() will not honor
    timeout but exits immediately with return code set to
    WL_LATCH_SET.
    
    Clearly approach 2 wins the race for all practical and production
    purposes and this commit implements the approach 2. However, it
    might be worth making per-db worker wait time configurable so that
    the tests can use a very low value to make tests faster and in
    production the default 180 sec can stay. But, that's for another
    day.
    
    [1]
    t/100_global_sequence_logical.pl .. 77/? # poll_query_until timed out executing this query:
    SELECT NOT EXISTS (SELECT 1 FROM bdr.bdr_node_slots WHERE node_name = 'node_b')
    expecting this output:
    t
    last actual query output:
    f
    with stderr:

--- bdr_perdb.c
+++ bdr_perdb.c
@@ -251,6 +251,7 @@ bdr_maintain_db_workers(void)
 	List	   *nodes_to_forget = NIL;
 	ListCell   *lcparted;
 	ListCell   *lcforget;
+	bool		found_alive_worker = false;
 
 	bdr_make_my_nodeid(&myid);
 
@@ -429,8 +430,7 @@ bdr_maintain_db_workers(void)
 
 		if (found_alive)
 		{
-			/* check again next time round, soon please */
-			SetLatch(&MyProc->procLatch);
+			found_alive_worker = true;
 
 			/*
 			 * and treat as still alive for DDL locking purposes, since if it
@@ -498,6 +498,19 @@ bdr_maintain_db_workers(void)
 			(void) MemoryContextSwitchTo(oldcontext);
 		}
 	}
+
+	/*
+	 * If at least one worker was found alive and killed in the above for loop,
+	 * we check again next time for dropping replication slots of the parted
+	 * peers. However, we want to do this soon, so setting the latch ensures
+	 * the per-db worker doesn't go into long wait in its main loop. And, we
+	 * set the latch specifically after ReplicationSlotDrop() call in the above
+	 * for loop, because it can get reset by ConditionVariablePrepareToSleep()
+	 * or ConditionVariableSleep() (called via ReplicationSlotDrop() ->
+	 * ReplicationSlotAcquire()) making per-db worker go into long wait.
+	 */
+	if (found_alive_worker)
+		SetLatch(&MyProc->procLatch);
 
 	PopActiveSnapshot();
 	SPI_finish();
