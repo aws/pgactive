a392941423cdc7056ebc761ed32dede765722f89
Bharath Rupireddy <120176237+rupireddAWS@users.noreply.github.com>
2023-08-28 21:21:36 +0530

    Allow user mapping and foreign server commands local execution (#172)
    This commit allows user mapping and foreign server commands local
    execution i.e. no replication to BDR members when BDR is active.
    This enables use of user mappings in BDR in place of DSNs/plain
    connection strings. For instance, to join a new node to BDR group
    one has to create forein server and user mapping for the new node
    on all of the existing BDR members on whom BDR is already active.
    Prior to this commit, creation of foreign server and user mapping
    fails when BDR is active.
    
    This commit also adds a TAP test for seamless failover of a BDR
    node to streaming standby with user mappings. An already existing
    TAP test uses DSNs/connection strings which updates BDR catalogs
    to reflect standby's connection string on all of the BDR members.
    Updating BDR catalogs (bdr_nodes and bdr_connections) seemed not
    so sophisticated when BDR supports user mappings and updating
    just the foreign server associated with user mapping is
    sufficient without touching BDR catalogs. A new TAP test is added
    to demonstrate the use of user mapping, updateing foreign server
    during failover.
    
    Co-authored-by: Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>

--- src/bdr_commandfilter.c
+++ src/bdr_commandfilter.c
@@ -1311,14 +1311,20 @@ bdr_commandfilter(PlannedStmt *pstmt,
 
 		case T_CreateFdwStmt:
 		case T_AlterFdwStmt:
+			/* XXX: we should probably support all of these at some point */
+			error_unsupported_command(GetCommandTagName(CreateCommandTag(parsetree)));
+			break;
+
+			/*
+			 * Execute the following only on local node i.e. no replication to
+			 * other BDR members.
+			 */
 		case T_CreateForeignServerStmt:
 		case T_AlterForeignServerStmt:
 		case T_CreateUserMappingStmt:
 		case T_AlterUserMappingStmt:
 		case T_DropUserMappingStmt:
-			/* XXX: we should probably support all of these at some point */
-			error_unsupported_command(GetCommandTagName(CreateCommandTag(parsetree)));
-			break;
+			goto done;
 
 		case T_CompositeTypeStmt:	/* CREATE TYPE (composite) */
 		case T_CreateEnumStmt:	/* CREATE TYPE AS ENUM */
@@ -1406,6 +1412,13 @@ bdr_commandfilter(PlannedStmt *pstmt,
 								(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 								 errmsg("DROP INDEX CONCURRENTLY is not supported without bdr.skip_ddl_replication set")));
 				}
+
+				/*
+				 * Execute the DROP SERVER only on local node i.e. no
+				 * replication to other BDR members.
+				 */
+				if (stmt->removeType == OBJECT_FOREIGN_SERVER)
+					goto done;
 			}
 			break;
 
