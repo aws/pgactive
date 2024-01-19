fa48bff39dc82fe813ad04adc05885a5876ba10d
Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
2024-01-19 08:17:03 +0000

    Bump version to 2.1.2

---
+++ pgactive--2.1.1--2.1.2.sql
@@ -0,0 +1,4 @@
+/* pgactive--2.1.1--2.1.2.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pgactive UPDATE TO '2.1.2'" to load this file. \quit
