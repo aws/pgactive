b5cc5667a3870e8253f09909f10b000143ece665
Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
2024-02-01 07:14:40 +0000

    Move read_only.sql tests to TAP

--- test/t/059_misc.pl
+++ test/t/059_misc.pl
@@ -18,7 +18,7 @@ my $nodes = make_pgactive_group(2,'node_');
 my ($node_0,$node_1) = @$nodes;
 
 $node_0->safe_psql($pgactive_test_dbname,
-    q[CREATE TABLE fruits(id integer, name varchar);]);
+    q[CREATE TABLE fruits(id integer PRIMARY KEY, name varchar);]);
 $node_0->safe_psql($pgactive_test_dbname,
     q[INSERT INTO fruits VALUES (1, 'Mango');]);
 wait_for_apply($node_0, $node_1);
@@ -49,11 +49,89 @@ wait_for_apply($node_0, $node_1);
 $node_0->poll_query_until($pgactive_test_dbname,
   qq[SELECT COUNT(*) = 2 FROM fruits;]);
 
+# Test the capability to set all pgactive nodes read-only
+# Set all nodes read-only
+$node_0->safe_psql($pgactive_test_dbname,
+  qq[SELECT pgactive.pgactive_set_node_read_only(node_name, true) FROM pgactive.pgactive_nodes;]);
+$node_0->poll_query_until($pgactive_test_dbname,
+  qq[SELECT node_read_only IS true FROM pgactive.pgactive_nodes WHERE node_name = 'node_0';]);
+$node_1->poll_query_until($pgactive_test_dbname,
+  qq[SELECT node_read_only IS true FROM pgactive.pgactive_nodes WHERE node_name = 'node_1';]);
+
+my $query = qq[CREATE TABLE readonly_test_shoulderror(a int);];
+my ($result, $stdout, $stderr) = ('','', '');
+($result, $stdout, $stderr) = $node_0->psql(
+    $pgactive_test_dbname,
+    $query);
+like($stderr, qr/.*ERROR.*cannot run CREATE TABLE on read-only pgactive node/,
+     "creation of table on node set to read-only fails");
+
+$query = qq[INSERT INTO fruits VALUES (3, 'Cherry');];
+($result, $stdout, $stderr) = $node_0->psql(
+    $pgactive_test_dbname,
+    $query);
+like($stderr, qr/.*ERROR.*INSERT may only affect UNLOGGED or TEMPORARY tables on read-only pgactive node; fruits is a regular table/,
+     "insertion into a table on node set to read-only fails");
+
+$query = qq[UPDATE fruits SET name = 'Berry' WHERE id = 1;];
+($result, $stdout, $stderr) = $node_1->psql(
+    $pgactive_test_dbname,
+    $query);
+like($stderr, qr/.*ERROR.*UPDATE may only affect UNLOGGED or TEMPORARY tables on read-only pgactive node; fruits is a regular table/,
+     "update of a table on node set to read-only fails");
+
+$query = qq[DELETE FROM fruits WHERE id = 1;];
+($result, $stdout, $stderr) = $node_0->psql(
+    $pgactive_test_dbname,
+    $query);
+like($stderr, qr/.*ERROR.*DELETE may only affect UNLOGGED or TEMPORARY tables on read-only pgactive node; fruits is a regular table/,
+     "delete from a table on node set to read-only fails");
+
+$query = qq[COPY public.test_read_only FROM '/tmp/nosuch.csv';];
+($result, $stdout, $stderr) = $node_1->psql(
+    $pgactive_test_dbname,
+    $query);
+like($stderr, qr/.*ERROR.*cannot run COPY FROM on read-only pgactive node/,
+     "COPY FROM on a table on node set to read-only fails");
+
+$query = qq[WITH cte AS (
+	INSERT INTO fruits VALUES (3, 'Cherry') RETURNING *
+)
+SELECT * FROM cte;];
+($result, $stdout, $stderr) = $node_0->psql(
+    $pgactive_test_dbname,
+    $query);
+like($stderr, qr/.*ERROR.*(DML|SELECT INTO) may only affect UNLOGGED or TEMPORARY tables on read-only pgactive node; fruits is a regular table/,
+     "CTE command on node set to read-only fails");
+
+# Temporary tables succeed even when node is set read-only
+$result = $node_0->safe_psql($pgactive_test_dbname,
+    q[CREATE TEMP TABLE test_read_only_temp(data text);
+      INSERT INTO test_read_only_temp VALUES('foo');
+      UPDATE test_read_only_temp SET data = 'foo';
+      DELETE FROM test_read_only_temp;
+      SELECT 'finished';
+    ]);
+is($result, 'finished', 'check if commands on temporary tables works even when node is set read-only');
+
+# Set all nodes read-write
+$node_0->safe_psql($pgactive_test_dbname,
+  qq[SELECT pgactive.pgactive_set_node_read_only(node_name, false) FROM pgactive.pgactive_nodes;]);
+$node_0->poll_query_until($pgactive_test_dbname,
+  qq[SELECT node_read_only IS false FROM pgactive.pgactive_nodes WHERE node_name = 'node_0';]);
+$node_1->poll_query_until($pgactive_test_dbname,
+  qq[SELECT node_read_only IS false FROM pgactive.pgactive_nodes WHERE node_name = 'node_1';]);
+
+$node_0->safe_psql($pgactive_test_dbname, q[DELETE FROM fruits;]);
+wait_for_apply($node_0, $node_1);
+
+$node_0->poll_query_until($pgactive_test_dbname,
+  qq[SELECT COUNT(*) = 0 FROM fruits;]);
+
 # The DB name pgactive_supervisordb is reserved by pgactive. None of these
 # commands may be permitted.
-my $query = qq[CREATE DATABASE pgactive_supervisordb;];
+$query = qq[CREATE DATABASE pgactive_supervisordb;];
 # Must not use safe_psql since we expect an error here
-my ($result, $stdout, $stderr) = ('','', '');
 ($result, $stdout, $stderr) = $node_0->psql(
     $pgactive_test_dbname,
     $query);
